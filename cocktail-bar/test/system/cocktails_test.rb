93c83b9208d2035d4fc6f854499144f14b9b76d2
Daniel Ribeiro <94848526+danielmribeiro@users.noreply.github.com>
2022-03-23 15:59:20 +0000

    Conflits solved

--- cocktail-bar/test/system/cocktails_test.rb
+++ cocktail-bar/test/system/cocktails_test.rb
@@ -3,20 +3,8 @@ require "application_system_test_case"
 class CocktailsTest < ApplicationSystemTestCase
   setup do
     @category = categories(:first)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    @cocktail = cocktails(:first)
->>>>>>> UI_03
-=======
     @cocktail = cocktails(:first)
     @ingredient = ingredients(:first)
->>>>>>> UI_11
-=======
-    @cocktail = cocktails(:first)
-    @ingredient = ingredients(:first)
->>>>>>> ROR_08
   end
 
   test "List categories" do
@@ -24,70 +12,31 @@ class CocktailsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Categories"
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  # test "List all cocktails from a category" do  end
-=======
-=======
->>>>>>> UI_11
-=======
->>>>>>> ROR_08
   test "List all cocktails from a category" do
     visit categories_path
     assert_selector "h1", text: "Categories"
 
     click_on @category.name
-<<<<<<< HEAD
-    assert_text @cocktail.name
-  end
-<<<<<<< HEAD
->>>>>>> UI_03
-=======
->>>>>>> UI_11
-=======
     assert_text @cocktail.name.titleize
   end
->>>>>>> ROR_08
 
   # test "List searched cocktails from a category" do  end
 
   # test "Create a new cocktail" do  end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  # test "List cocktail title in cocktail page" do  end
-
-  # test "List cocktail description in cocktail page" do  end
-
-  # test "List cocktail photo in cocktail page" do  end
-=======
-=======
->>>>>>> ROR_08
   test "List cocktail page" do
 
     visit categories_path
     assert_selector "h1", text: "Categories"
 
     click_on @category.name
-<<<<<<< HEAD
-    assert_text @cocktail.name
-
-    click_on @cocktail.name
-=======
     assert_text @cocktail.name.titleize
 
     click_on @cocktail.name.titleize
->>>>>>> ROR_08
     assert_text @cocktail.name.titleize
     assert_text @cocktail.instructions
     assert_text @ingredient.name.capitalize
   end
-<<<<<<< HEAD
->>>>>>> UI_11
-=======
->>>>>>> ROR_08
-
   # test "Add favourite to the cocktail" do  end
 
   # test "Remove favourite from the cocktail" do  end
@@ -102,13 +51,6 @@ class CocktailsTest < ApplicationSystemTestCase
 
   # test "List all ingredients from a cocktail" do  end
 
-<<<<<<< HEAD
-  # test "List ingredient title in ingredient page" do  end
-
-  # test "List ingredient description in ingredient page" do  end
-
-  # test "List ingredient photo in ingredient page" do  end
-=======
   test "List ingredient page" do
     visit categories_path
     assert_selector "h1", text: "Categories"
@@ -117,13 +59,10 @@ class CocktailsTest < ApplicationSystemTestCase
     assert_text @cocktail.name.titleize
 
     click_on @cocktail.name.titleize
-    assert_text @cocktail.name.titleize
-    assert_text @cocktail.instructions
     assert_text @ingredient.name.capitalize
 
-    click_on @ingredient.name.titleize
-    assert_text @ingredient.name.titleize
+    click_on @ingredient.name.capitalize
+    assert_text @ingredient.name.capitalize
     assert_text @ingredient.description
   end
->>>>>>> ROR_08
 end
